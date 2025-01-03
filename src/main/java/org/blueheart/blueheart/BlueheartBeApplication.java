3d88358298b00ce2e026323934ecffadd68c2ef9
yemin4242 <whereismysejong>
2025-01-04 03:20:52 +0900

    [BE] BUILD: Initial Spring Setup#1

---
+++ src/main/java/org/blueheart/blueheart/BlueheartBeApplication.java
@@ -0,0 +1,13 @@
+package org.blueheart.blueheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueheartBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueheartBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/blueheart/blueheart/BlueheartBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blueheart.blueheart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueheartBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
