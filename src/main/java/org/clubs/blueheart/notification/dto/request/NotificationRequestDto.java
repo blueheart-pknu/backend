d6d86452ec464c1d53badddbffca3540f2330087
yemin4242 <whereismysejong>
2025-01-17 21:09:15 +0900

    [BE] FEAT: Notification Dto Validation 추가 #14

--- src/main/java/org/clubs/blueheart/notification/dto/request/NotificationRequestDto.java
+++ src/main/java/org/clubs/blueheart/notification/dto/request/NotificationRequestDto.java
@@ -1,5 +1,6 @@
 package org.clubs.blueheart.notification.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,9 +8,16 @@ import lombok.Data;
 @Builder
 public class NotificationRequestDto {
 
+    @NotNull(message = "SenderID must not be blank")
+    @Min(value = 1, message = "송신자ID는 1이상이어야합니다.")
     private Long senderId;
 
+    @NotNull(message = "ReceiverID must not be blank")
+    @Min(value = 1, message = "수신자ID는 1이상이어야합니다.")
     private Long receiverId;
 
+    @NotBlank(message = "Content must not be blank")
+    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\uAC00-\\uD7A3\\u0020-\\u007E]*$", message="한글, 영어, 기호, 유니코드만 사용할 수 있습니다")
+    @Size(min = 1, max = 255, message = "내용은 255자 이하입니다")
     private String content;
 }
--- src/main/java/org/clubs/blueheart/notification/dto/response/NotificationResponseDto.java
+++ src/main/java/org/clubs/blueheart/notification/dto/response/NotificationResponseDto.java
@@ -1,5 +1,9 @@
 package org.clubs.blueheart.notification.dto.response;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,9 +12,15 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class NotificationResponseDto {
+    @NotBlank(message = "Content must not be blank")
+    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\uAC00-\\uD7A3\\u0020-\\u007E]*$", message="한글, 영어, 기호, 유니코드만 사용할 수 있습니다")
+    @Size(min = 1, max = 255, message = "내용은 255자 이하입니다")
     private String content;
 
+    @NotBlank(message = "SenderUsername must not be blank")
+    @Pattern(regexp = "^[a-zA-Zㄱ-ㅎ가-힣ㅏ-ㅣ]+$")
     private String senderUsername;
 
+    @NotBlank(message = "expiredAt must not be blank")
     private LocalDateTime createdAt;
 }
