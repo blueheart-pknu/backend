875488ebf772ecb5fe7f91a7777c0476f98fa2bb
yemin4242 <whereismysejong>
2025-01-30 22:32:11 +0900

    [BE] FEAT: Notification Dto 순서설정 추가 #27

--- src/main/java/org/clubs/blueheart/notification/dto/request/NotificationRequestDto.java
+++ src/main/java/org/clubs/blueheart/notification/dto/request/NotificationRequestDto.java
@@ -3,21 +3,30 @@ package org.clubs.blueheart.notification.dto.request;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
+import org.clubs.blueheart.config.ValidationGroups;
 
 @Data
 @Builder
 public class NotificationRequestDto {
 
-    @NotNull(message = "SenderID must not be null")
-    @Min(value = 1, message = "송신자ID는 1이상이어야합니다.")
+    @NotNull(message = "Sender ID must not be null",
+            groups = ValidationGroups.NotNullGroup.class)
+    @Min(value = 1, message = "Sender ID must be at least 1",
+            groups = ValidationGroups.SizeGroup.class)
     private Long senderId;
 
-    @NotNull(message = "ReceiverID must not be null")
-    @Min(value = 1, message = "수신자ID는 1이상이어야합니다.")
+    @NotNull(message = "Receiver ID must not be null",
+            groups = ValidationGroups.NotNullGroup.class)
+    @Min(value = 1, message = "Receiver ID must be at least 1",
+            groups = ValidationGroups.SizeGroup.class)
     private Long receiverId;
 
-    @NotBlank(message = "Content must not be blank")
-    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\uAC00-\\uD7A3\\u0020-\\u007E]*$", message="한글, 영어, 기호, 유니코드만 사용할 수 있습니다")
-    @Size(min = 1, max = 255, message = "내용은 255자 이하입니다")
+    @NotBlank(message = "Content must not be blank",
+            groups = ValidationGroups.NotBlankGroup.class)
+    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\uAC00-\\uD7A3\\u0020-\\u007E]*$",
+            message = "Content can only contain letters, numbers, and symbols",
+            groups = ValidationGroups.PatternGroup.class)
+    @Size(min = 1, max = 255, message = "Content cannot exceed 255 characters",
+            groups = ValidationGroups.SizeGroup.class)
     private String content;
 }
--- src/main/java/org/clubs/blueheart/notification/dto/response/NotificationResponseDto.java
+++ src/main/java/org/clubs/blueheart/notification/dto/response/NotificationResponseDto.java
@@ -3,21 +3,31 @@ package org.clubs.blueheart.notification.dto.response;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
+import org.clubs.blueheart.config.ValidationGroups;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 public class NotificationResponseDto {
-    @NotBlank(message = "Content must not be blank")
-    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\uAC00-\\uD7A3\\u0020-\\u007E]*$", message="한글, 영어, 기호, 유니코드만 사용할 수 있습니다")
-    @Size(min = 1, max = 255, message = "내용은 255자 이하입니다")
+
+    @NotBlank(message = "Content must not be blank",
+            groups = ValidationGroups.NotBlankGroup.class)
+    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\uAC00-\\uD7A3\\u0020-\\u007E]*$",
+            message = "Content can only contain letters, numbers, and symbols",
+            groups = ValidationGroups.PatternGroup.class)
+    @Size(min = 1, max = 255, message = "Content cannot exceed 255 characters",
+            groups = ValidationGroups.SizeGroup.class)
     private String content;
 
-    @NotBlank(message = "SenderUsername must not be blank")
-    @Pattern(regexp = "^[a-zA-Zㄱ-ㅎ가-힣ㅏ-ㅣ]+$")
+    @NotBlank(message = "Sender username must not be blank",
+            groups = ValidationGroups.NotBlankGroup.class)
+    @Pattern(regexp = "^[a-zA-Zㄱ-ㅎ가-힣ㅏ-ㅣ]+$",
+            message = "Sender username can only contain letters",
+            groups = ValidationGroups.PatternGroup.class)
     private String senderUsername;
 
-    @NotNull(message = "expiredAt must not be blank")
+    @NotNull(message = "Creation timestamp must not be null",
+            groups = ValidationGroups.NotNullGroup.class)
     private LocalDateTime createdAt;
 }
